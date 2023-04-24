053c5a1093ad0cd1a76a7dafb7d1c791e103f925
maikoncorrea <maikonacorrea@gmail.com>
2023-04-24 16:51:40 -0300

    alterações no bloco popup e imagens

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-let openPopupButton = document.querySelector('.button__edition');
-let popup = document.querySelector('.popup');
+let openPopupButton = document.querySelector('.button__edition');//nomeia o botão de edição
+let popup = document.querySelector('.popup__container');//nomeia o formulário a ser aberto
+let closePopupButton = document.querySelector('.button__image-close');//seleciona o
 
-openPopupButton.addEventListener('click', () => {
-  popup.style.display = 'block';
+openPopupButton.addEventListener('click', () => { // botão a ser clicado
+  popup.style.display = 'block'; // mudança de estado do bloco
 });
 
 popup.addEventListener('click', (event) => {
-  if (event.target === popup) {
+  if (event.target === closePopupButton) {
     popup.style.display = 'none';
   }
 });
