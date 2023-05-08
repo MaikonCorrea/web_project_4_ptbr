3fa970b3bc3ba7f483bab98e2df4e6b163878db1
maikoncorrea <maikonacorrea@gmail.com>
2023-05-08 17:07:10 -0300

    cards em JS adicionado/botão de exclusão adicionado

--- scripts/script.js
+++ scripts/script.js
@@ -32,15 +32,84 @@ infoName.textContent = inputName.value
   removeDisplayBlockPopupClass();
 }
 
-//botão para abertura do popup de adição de cards
+//chamando o botão de abrir popup card
 const openPopupButtonCard = document.querySelector('.button__add');
 const popupCard = document.querySelector('.popup__card');
-//botão de salvar card conferido
 const buttonAddCard = popupCard.querySelector('.button__save');
-
+const closePopupCardButton = popupCard.querySelector('.button__close-popup');
+const inputTitle = popupCard.querySelector('.input__name');
+const inputImageURL = popupCard.querySelector('.input__sobre');
 
 //função de abertura do popup card
 openPopupButtonCard.addEventListener('click', addDisplayBlockPopupClassCard)
 function addDisplayBlockPopupClassCard() {
   popupCard.classList.add('popup_opened-card');
 }
+
+//função de fechamento do popup card
+closePopupCardButton.addEventListener('click' , removeDisplayBlockPopupCardClass)
+function removeDisplayBlockPopupCardClass (event) {
+  popupCard.classList.remove('popup_opened-card');
+    event.preventDefaultCards();
+}
+
+// função add card
+buttonAddCard.addEventListener('click', saveNewImputCards)
+function saveNewImputCards (event) {
+  event.preventDefaultCards();
+  if (inputTitle.value != '') {
+    placeTitle.textContent = inputTitle.value
+  }
+
+  if (inputImageURL.value != '') {
+    inputImageURL.value
+  }
+  removeDisplayBlockPopupCardClass();
+}
+
+const initialCards = [
+  {
+    name: "Vale de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg"
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg"
+  },
+  {
+    name: "Montanhas Carecas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_bald-mountains.jpg"
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_latemar.jpg"
+  },
+  {
+    name: "Parque Nacional da Vanoise ",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg"
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg"
+  },
+];
+
+function createCard(initialCards) {
+  const cardTemplate = document.querySelector('#gallery__card').content;
+  const cardElement = cardTemplate.querySelector('.place').cloneNode(true);
+  const cardTitleElement = cardElement.querySelector('.place__title');
+  const cardImageElement = cardElement.querySelector('.place__image');
+  cardTitleElement.textContent = initialCards.name;
+  cardImageElement.src = initialCards.link;
+
+  return cardElement;
+}
+
+const cardsContainer = document.querySelector('.gallery');
+
+initialCards.forEach((card) => {
+  const cardElement = createCard(card);
+  cardsContainer.prepend(cardElement);
+});
+
+
