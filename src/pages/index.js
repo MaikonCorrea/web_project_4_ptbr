c431202479b0b40435f73eda46283538003b4c14
maikoncorrea <maikonacorrea@gmail.com>
2023-07-30 12:27:20 -0300

    restarting all

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,8 @@ import Section from "../components/Section.js";
 import Card from "../components/Card.js";
 import {initialCards, cardTemplate, cardsContainer} from "../utils/constants.js"
 import UserInfo from "../components/UserInfo.js";
+import PopupWithImage from "../components/PopupWithImage";
+
 
 const cardList = new Section({
   items: initialCards,
@@ -23,6 +25,23 @@ popupForm.setEventListeners();
 
 const popupUserInfo = new UserInfo('.edit');
 popupUserInfo.setEventListeners();
+
+/* const popupWithImage = new PopupWithImage("#popup-image");
+
+const cardScreen = new Card(
+  {
+    name: "Exemplo de Cartão",
+    link: "https://exemplo.com/imagem.jpg",
+    handleCardClick: (name,link) => {
+      popupWithImage.open(name, link);
+    }
+  },
+  cardTemplate
+);
+
+const cardTemplatescreen = cardScreen.generateCard(); */
+
+
 
 const validator = new FormValidation({
   inputErrorClass: "span__error",
