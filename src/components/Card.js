b2559ba0ecab0f90c6189760c785525b15f54c90
maikoncorrea <maikonacorrea@gmail.com>
2023-08-08 12:43:23 -0300

    card class change

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,13 @@
+import {owner} from "../utils/constants.js"
+
 export default class Card {
-  constructor({name, link}) {
-    this._name = name;
-    this._link = link;
+  constructor({item, templateSelector, deleteCard}) {
+    this._templateSelector = templateSelector;
+    this._id = item.id;
+    this._title = item.title;
+    this._url - item.link;
+    this._owner = item.owner;
+    this.deleteCard = deleteCard;
   };
 
   getTemplate() {
@@ -12,14 +18,23 @@ export default class Card {
 
   generateCard() {
     this._element = this.getTemplate();
-    this._element.querySelector(".place__image").src = `${this._link}`;
-    this._element.querySelector(".place__image").alt = `${this._name}`
-    this._element.querySelector(".place__title").textContent = this._name;
-    this._buttonLikeElement = this._element.querySelector(".place__button-like");
-    this._buttonDeleteCard = this._element.querySelector(".place__button-delete");
+    this._element.setAttribute("id", this._id);
+    this._element.setAttribute("owner_id", this._owner.id)
+    this._element.querySelector(".place__image").src = `${this._url}`;
+    this._element.querySelector(".place__image").alt = `${this._title}`
+    this._element.querySelector(".place__title").textContent = this._title;
     this.setEventListeners();
     return this._element;
   };
+
+  _removeCard(event) {
+    const checkId = owner.id == event.target.parentElement.getAttribute("owner_id");
+    if(!checkId) {
+      alert("Você só pode remover os seus Cards")
+      return
+    }
+    this.deleteCard()
+  }
 
   setEventListeners() {
     this._buttonLikeElement.addEventListener("click", () => {
