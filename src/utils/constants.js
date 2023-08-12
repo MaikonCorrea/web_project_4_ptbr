4ca658ca73f13b1007c8bf3adeeedd20a04fc035
maikoncorrea <maikonacorrea@gmail.com>
2023-08-11 22:32:55 -0300

    deleting and adding variables

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -25,8 +25,17 @@ export const buttonOpenEditImageProfile = document.querySelector(".profile__butt
 export const inputImageProfile = document.querySelector(".photograph__input-link");
 export const buttonSaveImageProfile = document.querySelector(".photograph__button-save");
 export const photographPopup = document.querySelector(".photograph");
+export const inputTitleInclude = document.querySelector(".include__input-title");
+export const inputUrlInclude = document.querySelector(".include__input-link");
+export const popupDelete = document.querySelector(".delete");
+export const openPopupDelete = document.querySelector(".place__button-delete");
+export const buttonDeleteCard = document.querySelector(".delete__button-save");
+
 export const owner = {
-  id: "e2bad784-3e1f-478a-b640-635d640e7341",
+  _id: "f382bd8051d8e7242391b643",
+  name: "Maikon Corrêa",
+  about: "Estudant, Developer",
+  avatar: "https://avatars.githubusercontent.com/u/121962633?u=8f6b79ed0efe033e91fa7d97adf7f41332ebc57f&v=4",
   cohort: "web_ptbr_05"
 }
 
@@ -40,34 +49,3 @@ export const configValidator = {
   inactiveButtonIncludeClass: "include__button-save_disabled",
   inactiveButtonPhotographClass: "photograph__button-save_disabled",
 }
-
-
-
-/*
-export const initialCards = [
-  {
-    name: "Yellostone National Park",
-    link: "https://images.unsplash.com/photo-1565108150023-cc0fd1054149?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80",
-  },
-  {
-    name: "Grand Canyon",
-    link: "https://images.unsplash.com/photo-1474044159687-1ee9f3a51722?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
-  },
-  {
-    name: "Colorado River",
-    link: "https://images.unsplash.com/photo-1589973488443-496846cae737?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1115&q=80",
-  },
-  {
-    name: "Latemar",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_latemar.jpg",
-  },
-  {
-    name: "Parque Nacional da Vanoise ",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg",
-  },
-  {
-    name: "Golden Gate Bridge",
-    link: "https://images.unsplash.com/photo-1558150503-0fdcce137289?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=715&q=80",
-  },
-]; */
-
