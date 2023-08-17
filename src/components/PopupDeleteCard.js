e2d996ae9e125c384b4e8eb7a98b1d8987f1c60f
maikoncorrea <maikonacorrea@gmail.com>
2023-08-17 13:51:23 -0300

    refactored popup extensions

--- src/components/PopupDeleteCard.js
+++ src/components/PopupDeleteCard.js
@@ -1,33 +1,17 @@
-import { confirmDeleteCard } from "../pages/index.js";
+import Popup from "./Popup.js";
 
-export default class PopupDeleteCard {
+export default class PopupDeleteCard extends Popup {
   constructor(popupSelector) {
-    this._handleEscClose = this._handleEscClose.bind(this);
-    this._popup = document.querySelector(".delete");
-    this._closePopup = this._popup.querySelector(".delete__button-close-popup");
-    this._confirmDelete = this._popup.querySelector(".delete__button-save");
+    super(popupSelector)
     this.setEventListeners();
   }
 
-  open() {
-    this._popup.classList.add("popup_opened");
-    document.addEventListener("keydown", this._handleEscClose);
-  }
-
   close(evt) {
-    this._popup.classList.add("popup_closing");
-    setTimeout(() => {
-      this._popup.classList.remove("popup_closing");
-      this._popup.classList.remove("popup_opened");
-    }, 200);
-    evt.preventDefault();
-    document.removeEventListener("keydown", this._handleEscClose);
+    super.close(evt)
   }
 
   _handleEscClose(evt) {
-    if (evt.key === "Escape") {
-      this.close(evt);
-    }
+    super._handleEscClose(evt)
   }
 
   renderLoading(isLoading) {
@@ -44,19 +28,6 @@ export default class PopupDeleteCard {
   }
 
   setEventListeners() {
-    this._closePopup.addEventListener("click", (evt) => {
-      this.close(evt);
-    });
-
-    this._popup.addEventListener("click", (evt) => {
-      const elementStyle = window.getComputedStyle(evt.target);
-      if (elementStyle.backgroundColor === "rgba(0, 0, 0, 0.5)") {
-        this.close(evt);
-      }
-    });
-    this._confirmDelete.addEventListener("click", (evt) => {
-      this.close(evt);
-      confirmDeleteCard(evt);
-    });
+    super.setEventListeners()
   }
 }
